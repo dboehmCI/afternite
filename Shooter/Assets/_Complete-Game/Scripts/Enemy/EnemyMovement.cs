9158233e1abec754a8b192f4de1112f100c67cad
Danny Boehm <daniel.boehm728@myci.csuci.edu>
2018-02-05 22:25:41 -0800

    Fixed Enemy 'Elephant' Pathing
    Enemy Elephant was not locating the player correctly and EnemyMovement was corrected. Elephant now tracks player down and attempts to trigger Player Death.

--- Shooter/Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
+++ Shooter/Assets/_Complete-Game/Scripts/Enemy/EnemyMovement.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 namespace CompleteProject
 {
@@ -8,16 +9,16 @@ namespace CompleteProject
         Transform player;               // Reference to the player's position.
         PlayerHealth playerHealth;      // Reference to the player's health.
         EnemyHealth enemyHealth;        // Reference to this enemy's health.
-        UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
-
+        NavMeshAgent nav;               // Reference to the nav mesh agent.
+       
 
         void Awake ()
         {
             // Set up the references.
-            player = GameObject.FindGameObjectWithTag ("Player").transform;
+            player = GameObject.Find("Player").transform;
             playerHealth = player.GetComponent <PlayerHealth> ();
             enemyHealth = GetComponent <EnemyHealth> ();
-            nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+            nav = GetComponent <NavMeshAgent> ();
         }
 
 
