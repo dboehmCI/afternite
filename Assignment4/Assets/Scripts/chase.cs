cf496a24e1ede72f2ed054673f687c577e6adcdd
Danny Boehm <daniel.boehm728@myci.csuci.edu>
2018-02-27 00:57:51 -0800

    Working on Assignment5
    - Added Non-Interactable AI to chop some wood.
    -  Added Interactive NPC (Bandit)

---
+++ Assignment4/Assets/Scripts/chase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chase : MonoBehaviour {
+    public Transform player;
+    static Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if (Vector3.Distance(player.position, this.transform.position) < 5) // Determines if NPC Sees Player
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("Walk", true);
+
+            if (direction.magnitude > 4) // Moves NPC to Player
+            {
+                this.transform.Translate(0, 0, 0.05f);
+                anim.SetBool("Walk", true);
+                
+            }
+            else
+            {
+               
+                anim.SetBool("Walk", false);
+            }
+
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
+	}
+}
