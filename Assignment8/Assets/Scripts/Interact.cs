aa2fc463e61c10b989483e2e4bf430c336e9d01c
Danny Boehm <daniel.boehm728@myci.csuci.edu>
2018-03-29 20:16:39 -0700

    Completed Assignment 8
    Notes in Readme

---
+++ Assignment8/Assets/Scripts/Interact.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    public GameObject npcDialogueBox;
+    public Dialogue dialogue;
+
+    private void Awake()
+    {
+        npcDialogueBox.SetActive(false);
+    }
+
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "checkNPC")
+        {
+            npcDialogueBox.SetActive(true);
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
+
+    }
+}
