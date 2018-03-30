43fbf891f3c8b07ec3825ecbb849ec67ee1d19d1
Danny Boehm <daniel.boehm728@myci.csuci.edu>
2018-03-29 20:24:11 -0700

    Merge branch 'Assignment3' into Assignment8

--- Assignment3/Assets/Scripts/respawn.cs
+++ Assignment3/Assets/Scripts/respawn.cs
@@ -6,62 +6,19 @@ using UnityEngine.SceneManagement;
 public class respawn : MonoBehaviour {
 
 
-    private bool checkpointCollide = false;
-    private bool showSavemsg = true;
     public static int levelN = 0;
 
     private Vector3 startPos;
     private Quaternion startRot;
 
-
-    public GameObject Interact;
-   
-
-    private void Awake()
-    {
-        Interact.SetActive(false);
-    }
-
-     void Update()
-    {
-        if (checkpointCollide)
-        {
-            if (showSavemsg)
-            Interact.SetActive(true);
-
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    showSavemsg = false;
-                    Interact.SetActive(false);
-                }
-            
-        }
-    }
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 
         startPos = transform.position;
         startRot = transform.rotation;
 
 	}
-
-    private void OnTriggerStay(Collider col)
-    {
-       if  (showSavemsg == false)
-        {
-            startPos = col.transform.position;
-            startRot = col.transform.rotation;
-            Destroy(col.gameObject);
-        }
-    }
-
-    private void OnTriggerExit(Collider col)
-    {
-        checkpointCollide = false;
-      
-        Interact.SetActive(false);
-     
-    }
+	
     // Update is called once per frame
     void OnTriggerEnter (Collider col) {
 
@@ -76,20 +33,15 @@ public class respawn : MonoBehaviour {
         }
         else if(col.tag == "checkpoint")
         {
-               
-
-            checkpointCollide = true;
-
-    
-
+            startPos = col.transform.position;
+            startRot = col.transform.rotation;
+            Destroy(col.gameObject);
         }
         else if(col.tag =="thirdPersonGoal")
         {
-           
             Destroy(col.gameObject);
             GetComponent<Animator>().Play("WIN00", -1, 0f);
            
         }
     }
-  
 }
